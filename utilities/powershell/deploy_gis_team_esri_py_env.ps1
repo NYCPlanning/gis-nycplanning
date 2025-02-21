cef94a6b8392b94cc9696180715bd5e2aff6425a
Jack Rosacker <jrosacker@planning.nyc.gov>
2025-02-21 11:25:44 -0500

    initial commit

---
+++ utilities/powershell/deploy_gis_team_esri_py_env.ps1
@@ -0,0 +1,53 @@
+<#
+    Title: Deploy GIS Team Esri Python Environment
+    Purpose: 
+        Clones the default Python environment, uses a standardized naming convention,
+        installs packages such as geopandas to supplement the default packages, and attempts
+        to adjust dependency versions (i.e. numpy for geopandas)
+    Author: J Rosacker
+    Date: 2025-02-21
+    Notes: 
+        The script assumes that the origin env uses Python 3.9.16, and that the subsequent
+        numpy version to satisfy geopandas is 1.22. These will change as we upgrade to higher 
+        versions of ArcGIS Pro.
+#>
+$newEnvName = 'gis-team-default-env'
+$baseEnvName = 'arcgispro-py3'
+$numpyVersion = 1.22
+
+try {
+    Write-Output "`r`n>>> Checking conda version..."
+    conda --version
+}
+catch {
+    $message = @"
+
+>>> Encountered an error. Is conda initialized for your PowerShell? If not:
+
+1. Run the two commands below in PowerShell (copy/paste the two lines in their entirety into PS and run): 
+    set-executionpolicy remotesigned -scope currentuser
+    & 'C:\Program Files\ArcGIS\Pro\bin\Python\condabin\conda.bat' init
+
+2. Open a new PowerShell window (or close and restart) and run this deployment script again
+
+More info: https://www.esri.com/arcgis-blog/products/arcgis-pro/developers/conda-init-and-arcgis-pro/
+
+"@
+    Write-Output $message
+    Write-Output ">>> Error message: " $_
+    break 
+}
+Write-Output "`r`n>>> Cloning $baseEnvName into $newEnvName..."
+conda create --name $newEnvName --clone $baseEnvName
+
+Write-Output "`r`n>>> Activating $newEnvName..."
+conda activate $newEnvName
+
+Write-Output "`r`n>>> pip installing geopandas..."
+pip install geopandas
+
+Write-Output "`r`n>>> Dropping numpy version to $numpyVersion to satisfy geopandas dependencies..."
+pip install --user numpy==$numpyVersion
+
+Write-Output "`r`n>>> Listing all conda environments..."
+conda env list
