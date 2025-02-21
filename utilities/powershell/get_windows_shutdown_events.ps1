93123109fe64e9abbbe152da02b5a76bcfb46cfe
Jack Rosacker <jrosacker@planning.nyc.gov>
2025-02-21 17:29:31 -0500

    Create get_windows_shutdown_events.ps1

---
+++ utilities/powershell/get_windows_shutdown_events.ps1
@@ -0,0 +1,14 @@
+<#
+    Title: Get Windows Shutdown Events
+    Purpose: 
+        Queries the Windows Event Log and returns shutdown events, formatted.
+    Author: J Rosacker
+    Date: 2024-06-03
+    Notes: 
+    References:
+        https://devblogs.microsoft.com/scripting/use-powershell-to-parse-event-log-for-shutdown-events/
+        https://learn.microsoft.com/en-us/windows/win32/shutdown/system-shutdown-reason-codes?redirectedfrom=MSDN
+#>
+
+
+Get-EventLog -LogName system -Source user32 | Select-Object TimeGenerated, Message | Sort-Object message | Format-Table -Wrap
