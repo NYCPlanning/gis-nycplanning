3fc765afe154620c65eb9191fddd2b107bbb5342
Jack Rosacker <jrosacker@planning.nyc.gov>
2025-03-24 16:52:23 -0400

    add placeholder notes

--- src/dcpgis/logging.py
+++ src/dcpgis/logging.py
@@ -1,11 +1,24 @@
 import logging
 from pathlib import Path
 
-print("hello, i'm a logging module")
+print("hello, i'm a logging module")    #temp msg, confirms import during package development
 
 def set_logger(
     file: Path, mode: str = "a", format: str = "{asctime} - {levelname} - {message}"
 ):
+    """
+    ***
+    THIS IS A PLACEHOLDER FN, to help with package development.
+    ***
+    
+    Args:
+        file (Path): _description_
+        mode (str, optional): _description_. Defaults to "a".
+        format (str, optional): _description_. Defaults to "{asctime} - {levelname} - {message}".
+
+    Returns:
+        _type_: _description_
+    """
     logger = logging.getLogger(__name__)
     console_handler = logging.StreamHandler()
     file_handler = logging.FileHandler(file, mode=mode, encoding="utf-8")
