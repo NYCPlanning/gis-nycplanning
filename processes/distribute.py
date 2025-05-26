1df226055db71509ae82016a4edeaea2b64a9ce8
Jack Rosacker <jrosacker@planning.nyc.gov>
2025-05-25 21:31:53 -0400

    change log mesg

--- processes/distribute.py
+++ processes/distribute.py
@@ -4,7 +4,7 @@ import logging
 def run(process, product):
     def announce_self():
         logging.info(
-            f"this is the {__name__} module, ready to distribute {product.upper()}"
+            f"Hi! This is the {__name__} module, ready to distribute {product.upper()}"
         )
 
     announce_self()
