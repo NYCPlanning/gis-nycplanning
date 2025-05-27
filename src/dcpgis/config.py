09a7d298586e695687204617f9bc7ca2089749d6
Jack Rosacker <jrosacker@planning.nyc.gov>
2025-05-27 16:41:46 -0400

    fix Union syntax

--- src/dcpgis/config.py
+++ src/dcpgis/config.py
@@ -7,7 +7,7 @@ import yaml
 
 class Config:
     #TODO: move all open() calls to Path.open(), per ruff ruleset
-    def __init__(self, app_env: str, config_file_path: Union(Path, str)):
+    def __init__(self, app_env: str, config_file_path: Union[Path, str]):
         self.app_env = app_env
         self.config_file_path = config_file_path
 
