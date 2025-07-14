84a80941a7f25eead5221b751fd5d6d0b5f4ea27
Jack Rosacker <jrosacker@planning.nyc.gov>
2025-07-14 14:33:52 -0400

    create global cli
    create global cli
    
    rename; scaffold out as a module
    
    add override
    
    scaffold out as callable by cli
    
    overhaul: move files, add cli with global and process args, stub out disbtribute.py
    
    f
    
    restructure
    
    update --which flag to --datasets
    
    add help messages

--- src/dcpgis/utils/config.py
+++ src/dcpgis/utils/config.py
@@ -6,7 +6,7 @@ import yaml
 
 
 class Config:
-    #TODO: move all open() calls to Path.open(), per ruff ruleset
+    # TODO: move all open() calls to Path.open(), per ruff ruleset
     def __init__(self, app_env: str, config_file_path: Union[Path, str]):
         self.app_env = app_env
         self.config_file_path = config_file_path
@@ -30,4 +30,4 @@ class Config:
         return configs
 
     def get_config_from_xlsx(self) -> dict:
-        ...
+        return {}
