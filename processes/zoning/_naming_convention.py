cee3b62b21ab84c0da887bb7865e1c47d0e50461
Casey Smith <CSmith@planning.nyc.gov>
2025-08-27 11:05:21 -0400

    moved naming convention dicts to py file

---
+++ processes/zoning/_naming_convention.py
@@ -0,0 +1,33 @@
+zoning_naming_conventions = {
+    "nyco": {
+        "trd_source": "DZM_nyco",
+        "external_output": "nyc_zoning_commercial_overlays",
+    },
+    "nylh": {
+        "trd_source": "DZM_nylh",
+        "external_output": "nyc_zoning_limit_height",
+    },
+    "nysp": {
+        "trd_source": "DZM_nysp",
+        "external_output": "nyc_zoning_special_districts",
+    },
+    "nysp_sd": {
+        "trd_source": "DZM_nysp_sd",
+        "external_output": "nyc_zoning_special_subdistricts",
+    },
+    "nyzd": {
+        "trd_source": "DZM_nyzd",
+        "external_output": "nyc_zoning_districts",
+    },
+    "nyzma": {
+        "trd_source": "DZM_nyzi",
+        "external_output": "nyc_zoning_map_amendments",
+    },
+}
+
+georef_naming_conventions = {
+    "georeferenced_zoning_maps": {
+        "trd_source": "NYC_Zoning_Maps",
+        "external_output": "nyc_georeferenced_zoning_maps",
+    }
+}
