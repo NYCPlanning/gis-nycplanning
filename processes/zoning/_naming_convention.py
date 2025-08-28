cb276b0d6263050ac6144294037e78072df48280
Casey Smith <CSmith@planning.nyc.gov>
2025-08-28 15:10:23 -0400

    key name changes

--- processes/zoning/_naming_convention.py
+++ processes/zoning/_naming_convention.py
@@ -1,33 +1,33 @@
 ZONING_CONVENTIONS = {
     "nyco": {
-        "trd_source": "DZM_nyco",
-        "external_output": "nyc_zoning_commercial_overlays",
+        "trd_fc_name": "DZM_nyco",
+        "public_output_name": "nyc_zoning_commercial_overlays",
     },
     "nylh": {
-        "trd_source": "DZM_nylh",
-        "external_output": "nyc_zoning_limit_height",
+        "trd_fc_name": "DZM_nylh",
+        "public_output_name": "nyc_zoning_limit_height",
     },
     "nysp": {
-        "trd_source": "DZM_nysp",
-        "external_output": "nyc_zoning_special_districts",
+        "trd_fc_name": "DZM_nysp",
+        "public_output_name": "nyc_zoning_special_districts",
     },
     "nysp_sd": {
-        "trd_source": "DZM_nysp_sd",
-        "external_output": "nyc_zoning_special_subdistricts",
+        "trd_fc_name": "DZM_nysp_sd",
+        "public_output_name": "nyc_zoning_special_subdistricts",
     },
     "nyzd": {
-        "trd_source": "DZM_nyzd",
-        "external_output": "nyc_zoning_districts",
+        "trd_fc_name": "DZM_nyzd",
+        "public_output_name": "nyc_zoning_districts",
     },
     "nyzma": {
-        "trd_source": "DZM_nyzi",
-        "external_output": "nyc_zoning_map_amendments",
+        "trd_fc_name": "DZM_nyzi",
+        "public_output_name": "nyc_zoning_map_amendments",
     },
 }
 
 GEOREF_CONVENTIONS = {
     "georeferenced_zoning_maps": {
-        "trd_source": "NYC_Zoning_Maps",
-        "external_output": "nyc_georeferenced_zoning_maps",
+        "trd_fc_name": "NYC_Zoning_Maps",
+        "public_output_name": "nyc_georeferenced_zoning_maps",
     }
 }
