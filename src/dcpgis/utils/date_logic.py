30944b32ec460bb8dd8813e688245668cc5b3fe5
Casey Smith <CSmith@planning.nyc.gov>
2025-09-04 09:23:29 -0400

    added get_latest_date_from_field()

---
+++ src/dcpgis/utils/date_logic.py
@@ -0,0 +1,24 @@
+import arcpy
+
+def get_latest_date_from_field(feature_class_path: str, date_field: str) -> str:
+    """
+    Retrieve the latest date from a specified date field in an ArcGIS feature class.
+
+    Args:
+        feature_class_path (str): The path to the feature class to search.
+        date_field (str): The name of the date field to query for the latest date.
+
+    Returns:
+        str: The latest date in YYYYMMDD format, or None if no date is found.
+    """
+    latest_date = None
+    with arcpy.da.SearchCursor(
+        in_table=feature_class_path,
+        field_names=[date_field],
+        # sql_clause=("TOP 1", f"ORDER BY {date_field} DESC")
+    ) as cursor:
+        for row in cursor:
+            if row[0] is not None:
+                if latest_date is None or row[0] > latest_date:
+                    latest_date = row[0]
+    return latest_date.strftime("%Y%m%d") if latest_date else None
