3f02024bf87dd6ea763c583f3c5775f37e1e1cc9
Casey Smith <CSmith@planning.nyc.gov>
2025-09-04 09:24:14 -0400

    added trd_full_fc_path key value to ZONING_CONVENTIONS; may become obsolete eventually

--- processes/zoning/_naming_convention.py
+++ processes/zoning/_naming_convention.py
@@ -1,26 +1,32 @@
 ZONING_CONVENTIONS = {
     "nyco": {
         "trd_fc_name": "DZM_nyco",
+        "trd_full_fc_name": "GISTRD.TRD.DZM_nyco",
         "public_output_name": "nyc_zoning_commercial_overlays",
     },
     "nylh": {
         "trd_fc_name": "DZM_nylh",
+        "trd_full_fc_name": "GISTRD.TRD.DZM_nylh",
         "public_output_name": "nyc_zoning_limit_height",
     },
     "nysp": {
         "trd_fc_name": "DZM_nysp",
+        "trd_full_fc_name": "GISTRD.TRD.DZM_nysp",
         "public_output_name": "nyc_zoning_special_districts",
     },
     "nysp_sd": {
         "trd_fc_name": "DZM_nysp_sd",
+        "trd_full_fc_name": "GISTRD.TRD.DZM_nysp_sd",
         "public_output_name": "nyc_zoning_special_subdistricts",
     },
     "nyzd": {
         "trd_fc_name": "DZM_nyzd",
+        "trd_full_fc_name": "GISTRD.TRD.DZM_nyzd",
         "public_output_name": "nyc_zoning_districts",
     },
     "nyzma": {
         "trd_fc_name": "DZM_nyzi",
+        "trd_full_fc_name": "GISTRD.TRD.DZM_nyzi",
         "public_output_name": "nyc_zoning_map_amendments",
     },
 }
