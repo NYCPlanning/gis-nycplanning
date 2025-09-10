781561ce10bab2f5ebd03399cf18642f04726603
Casey Smith <CSmith@planning.nyc.gov>
2025-09-10 11:51:21 -0400

    added date_logic.calc_open_data_cycle_month()

--- src/dcpgis/utils/date_logic.py
+++ src/dcpgis/utils/date_logic.py
@@ -1,4 +1,7 @@
 import arcpy
+import logging
+from datetime import datetime, date, timedelta
+
 
 def get_latest_date_from_field(feature_class_path: str, date_field: str) -> str:
     """
@@ -21,3 +24,20 @@ def get_latest_date_from_field(feature_class_path: str, date_field: str) -> str:
                 if latest_date is None or row[0] > latest_date:
                     latest_date = row[0]
     return latest_date.strftime("%Y%m%d") if latest_date else None
+
+def calc_open_data_cycle_month(config_date: str) -> str:
+    """
+    Calculate a YYYYMM date string representing the open data cycle month
+    Can override this calculation by entering a YYYYMM date string into the config file
+    Source: https://stackoverflow.com/a/9725093
+    """
+    if config_date is None:
+        logging.debug("Date field from config file is blank - calculating YYYYMM from today's date")
+        today = date.today()
+        first_of_this_month = today.replace(day=1)
+        last_month = first_of_this_month - timedelta(days=1)
+        last_month = last_month.strftime("%Y%m")
+        return last_month
+    else:
+        logging.debug("Pulling YYYYMM date from date field in config file")
+        return str(config_date)
