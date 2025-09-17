6e6b5ea461d71510b2443e6227732f946eb0db8a
Casey Smith <CSmith@planning.nyc.gov>
2025-09-17 12:15:26 -0400

    Added export_features_..() and schema_print()

--- processes/zoning/utils.py
+++ processes/zoning/utils.py
@@ -1,6 +1,21 @@
-from datetime import date, timedelta
 import logging
+from pathlib import Path
+import arcpy
 
 def utils_test():
-    logging.info("Utils test is functioning properly.")
+    logging.debug("Utils test is functioning properly.")
 
+def export_features_using_dict(src: str, dst: str, dict_name: dict, src_key: str, dst_key: str): 
+    for key, value in dict_name.items():
+        src_path = str(Path(src) / value[src_key])
+        dst_path = str(Path(dst) / value[dst_key])
+
+        arcpy.conversion.ExportFeatures(in_features=src_path,
+                                        out_features=dst_path)
+
+def schema_print(path):
+    fc_fields=arcpy.ListFields(path)
+    
+    logging.debug(f"Fields in {path}:")
+    for field in fc_fields:
+        logging.debug(f"    Field Name: {field.name}, Field Type: {field.type}")
