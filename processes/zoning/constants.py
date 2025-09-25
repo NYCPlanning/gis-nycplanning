b7d605d59f7b40901378ba832858ae455ec42928
Casey Smith <CSmith@planning.nyc.gov>
2025-09-25 10:14:26 -0400

    added SQL expression handling to initial export

--- processes/zoning/constants.py
+++ processes/zoning/constants.py
@@ -5,6 +5,7 @@ ZONING_CONVENTIONS = {
         "public_output_name": "nyc_zoning_commercial_overlays",
         "keep_fields": ["OBJECTID", "Shape", "OVERLAY", "Shape_Length", "Shape_Area"],
         "rename_fields": {},
+        "sql_expression":""
     },
     "nylh": {
         "trd_fc_name": "DZM_nylh",
@@ -12,6 +13,7 @@ ZONING_CONVENTIONS = {
         "public_output_name": "nyc_zoning_limited_height",
         "keep_fields": ["OBJECTID", "Shape", "LHNAME", "LHLBL","Shape_Length", "Shape_Area"],
         "rename_fields": {},
+        "sql_expression":""
     },
     "nysp": {
         "trd_fc_name": "DZM_nysp",
@@ -19,6 +21,7 @@ ZONING_CONVENTIONS = {
         "public_output_name": "nyc_zoning_special_districts",
         "keep_fields": ["OBJECTID", "Shape", "SDNAME", "SDLBL", "Shape_Length", "Shape_Area"],
         "rename_fields": {},
+        "sql_expression":""
     },
     "nysp_sd": {
         "trd_fc_name": "DZM_nysp_sd",
@@ -26,6 +29,7 @@ ZONING_CONVENTIONS = {
         "public_output_name": "nyc_zoning_special_subdistricts",
         "keep_fields": ["OBJECTID", "Shape", "SPNAME", "SPLBL", "SUBDIST", "SUBDIST_LBL", "SUBAREA_LBL", "SUBAREA_OTR","Shape_Length", "Shape_Area"],
         "rename_fields": {},
+        "sql_expression":"SUBDIST IS NOT NULL"
     },
     "nyzd": {
         "trd_fc_name": "DZM_nyzd",
@@ -33,6 +37,7 @@ ZONING_CONVENTIONS = {
         "public_output_name": "nyc_zoning_districts",
         "keep_fields": ["OBJECTID", "Shape", "ZONEDIST", "Shape_Length", "Shape_Area"],
         "rename_fields": {},
+        "sql_expression":""
     },
     "nyzma": {
         "trd_fc_name": "DZM_nyzi",
@@ -40,6 +45,7 @@ ZONING_CONVENTIONS = {
         "public_output_name": "nyc_zoning_map_amendments",
         "keep_fields": ["OBJECTID", "Shape", "EFFECTIVE", "STATUS", "ULURPNO", "LUCATS", "PROJECT_NAME", "Shape_Length", "Shape_Area"],
         "rename_fields": {},
+        "sql_expression":"EFFECTIVE >= '2002-01-30 00:00:00' AND STATUS = '1' OR STATUS = '2'"
     },
 }
 
