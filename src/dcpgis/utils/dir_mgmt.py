20cd32b2ae0b2723e511910a38e2551b2e221098
Casey Smith <CSmith@planning.nyc.gov>
2025-12-11 10:50:29 -0500

    shutil copytree incorporated more directly

--- src/dcpgis/utils/dir_mgmt.py
+++ src/dcpgis/utils/dir_mgmt.py
@@ -14,17 +14,3 @@ def create_dir_with_subdirs(parent_dir_path: Path, sub_dirs: list) -> None:
     for sub_dir in sub_dirs:
         dir_path = Path(parent_dir_path / sub_dir)
         os.makedirs(dir_path)
-
-
-def copytree_overwrite(src: Path, dst: Path) -> None:
-    """
-    Copy contents of src directory to dst directory, overwriting existing files.
-    Args:
-        src (Path): The source directory to copy from.
-        dst (Path): The destination directory to copy to.
-    Returns:
-        None
-    """
-    if dst.exists():
-        shutil.rmtree(dst)  # Remove existing dir tree to avoid errors
-    shutil.copytree(src, dst)
