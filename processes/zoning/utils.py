8e98f1f77b4ddd470d5a69507173df9c7fb343d0
Casey Smith <CSmith@planning.nyc.gov>
2026-08-06 15:54:42 -0400

    ran ruff

--- processes/zoning/utils.py
+++ processes/zoning/utils.py
@@ -8,8 +8,10 @@ from arcpy import metadata as md
 
 logger = logging.getLogger(__name__)
 
+
 def utils_test():
     logger.debug("Utils test is functioning properly.")
+
 
 def export_feature_using_dict(
     src: str,
@@ -23,14 +25,14 @@ def export_feature_using_dict(
     drop_global_id: bool = False,
 ):
     """
-    Exports feature classes from a source to a destination using a dictionary to define parameters. 
-    
+    Exports feature classes from a source to a destination using a dictionary to define parameters.
+
     Args:
-        src (str): The source path where the feature classes are located.   
+        src (str): The source path where the feature classes are located.
         dst (str): The destination path where the feature classes will be exported. Destination can be a gdb or dir. If dst is a dir, set export_as_shapefile=True.
         dict_name (dict): A dictionary containing the parameters for each feature class to be exported.
         src_key (str): The key in the dictionary that contains the source feature class name.
-        dst_key (str): The key in the dictionary that contains the destination feature class name.  
+        dst_key (str): The key in the dictionary that contains the destination feature class name.
         src_prefix (str, optional): A prefix to be added to the source feature class names. Defaults to an empty string.
         sql_key (str, optional): The key in the dictionary that contains an optional SQL expression for filtering features during export. Defaults to None.
         export_as_shapefile (bool, optional): If True, exports the features as shapefiles. Defaults to False.
@@ -50,27 +52,31 @@ def export_feature_using_dict(
     else:
         fms = None
 
-    if sql_key is None: 
-        arcpy.conversion.ExportFeatures(in_features=src_path,
-                                        out_features=dst_path,
-                                        field_mapping=fms,
-                                        )
+    if sql_key is None:
+        arcpy.conversion.ExportFeatures(
+            in_features=src_path,
+            out_features=dst_path,
+            field_mapping=fms,
+        )
     else:
-        arcpy.conversion.ExportFeatures(in_features=src_path,
-                                        out_features=dst_path,
-                                        where_clause=feature_info[sql_key],
-                                        field_mapping=fms,
-                                        )
-    
-    in_count, out_count = inspect_data.get_record_count_comparison(dataset_1=src_path,
-                                                    dataset_2=dst_path)
-    
+        arcpy.conversion.ExportFeatures(
+            in_features=src_path,
+            out_features=dst_path,
+            where_clause=feature_info[sql_key],
+            field_mapping=fms,
+        )
+
+    in_count, out_count = inspect_data.get_record_count_comparison(dataset_1=src_path, dataset_2=dst_path)
+
     if out_count != in_count:
-        logger.debug(f"Record count of {os.path.basename(dst_path)} changed from {in_count} to {out_count} during processing")
+        logger.debug(
+            f"Record count of {os.path.basename(dst_path)} changed from {in_count} to {out_count} during processing"
+        )
+
 
 def drop_fields_from_fc(workspace: str, feature_class: str, keep_fields: list):
     """Drops all fields from a feature class except those specified in keep_fields.
-    
+
     Args:
         workspace (str): The path to the workspace containing the feature class.
         feature_class (str): The name of the feature class from which to drop fields.
@@ -82,13 +88,12 @@ def drop_fields_from_fc(workspace: str, feature_class: str, keep_fields: list):
     fields_to_delete = [field for field in all_fields if field not in keep_fields and field not in exceptions]
 
     if fields_to_delete:
-        arcpy.management.DeleteField(in_table=feature_class,
-                                     drop_field=fields_to_delete)
+        arcpy.management.DeleteField(in_table=feature_class, drop_field=fields_to_delete)
 
 
 def dissolve_in_place(workspace: str, feature_class: str, dissolve_field: list, statistics_fields: list[list]):
     """
-    Dissolves a feature class in place based on specified fields and statistics. 
+    Dissolves a feature class in place based on specified fields and statistics.
 
     Args:
         workspace (str): The path to the workspace containing the feature class.
@@ -97,20 +102,21 @@ def dissolve_in_place(workspace: str, feature_class: str, dissolve_field: list, 
         statistics_fields (list[list]): A list of lists that include statistics fields for dissolve. Example: [["Shape_Length", "SUM"], ["Shape_Area", "SUM"]]
     """
     arcpy.env.workspace = workspace
-    arcpy.management.Rename(in_data=feature_class, 
-                            out_data=f"{feature_class}_UNDISSOLVED")
-    
-    arcpy.management.Dissolve(in_features=f"{feature_class}_UNDISSOLVED", 
-                              out_feature_class=feature_class,
-                              dissolve_field=dissolve_field, 
-                              statistics_fields=statistics_fields,
-                            )
-    
-    in_count, out_count = inspect_data.get_record_count_comparison(dataset_1=f"{feature_class}_UNDISSOLVED",
-                                                                    dataset_2=feature_class)
+    arcpy.management.Rename(in_data=feature_class, out_data=f"{feature_class}_UNDISSOLVED")
+
+    arcpy.management.Dissolve(
+        in_features=f"{feature_class}_UNDISSOLVED",
+        out_feature_class=feature_class,
+        dissolve_field=dissolve_field,
+        statistics_fields=statistics_fields,
+    )
+
+    in_count, out_count = inspect_data.get_record_count_comparison(
+        dataset_1=f"{feature_class}_UNDISSOLVED", dataset_2=feature_class
+    )
     if out_count != in_count:
-            logger.debug(f"Record count of {feature_class} changed from {in_count} to {out_count} during processing")
-    
+        logger.debug(f"Record count of {feature_class} changed from {in_count} to {out_count} during processing")
+
     # Drop statistics type prefix from field name to retain original schema
     fields_in_fc = [f.name for f in arcpy.ListFields(feature_class)]
     for field_name in fields_in_fc:
@@ -125,16 +131,16 @@ def dissolve_in_place(workspace: str, feature_class: str, dissolve_field: list, 
     arcpy.management.Delete(in_data=f"{feature_class}_UNDISSOLVED")
 
 
-def update_metadata_values(base_dict: dict, feature_info: dict, cycle_date: str, council_date:str) -> dict:
+def update_metadata_values(base_dict: dict, feature_info: dict, cycle_date: str, council_date: str) -> dict:
     """
     Creates an updated metadata dictionary for a specific feature.
-    
+
     Args:
         base_dict (dict): Base metadata dictionary with default values
         feature_info (dict): Dictionary containing feature-specific information
         cycle_date (str): Publication date for the dataset
         council_date (str): Council approval date
-    
+
     Returns:
         dict: Updated metadata dictionary for this feature
     """
@@ -145,11 +151,11 @@ def update_metadata_values(base_dict: dict, feature_info: dict, cycle_date: str,
         "pub_date": cycle_date if cycle_date else "",
         "council_date": council_date if council_date else "",
         "item_name": feature_info["meta_res_title"],
-        "res_title": feature_info["meta_res_title"]
+        "res_title": feature_info["meta_res_title"],
     }
 
     metadata_values.update(updates)
-    return metadata_values    
+    return metadata_values
 
 
 def unpack_dict_into_string_file(input_str_path: str, output_str_path: str, value_dict: dict):
@@ -161,22 +167,22 @@ def unpack_dict_into_string_file(input_str_path: str, output_str_path: str, valu
         output_str_path (str): Path to the desired text output.
         value_dict (dict): Dictionary containing values to insert.
     """
-    
+
     # Read the XML as text
-    with open(input_str_path, 'r') as f:
+    with open(input_str_path, "r") as f:
         str_content = f.read()
 
     # # Replace placeholders with values
     str_content = str_content.format(**value_dict)
     # Write the modified content
-    with open(output_str_path, 'w', encoding='utf-8') as f:
+    with open(output_str_path, "w", encoding="utf-8") as f:
         f.write(str_content)
 
 
 def import_and_clean_feature_metadata(in_feature: str, md_template_file: str):
     """
     Imports metadata from a template into a feature class or shapefile and removes machine-specific information.
-    
+
     Upgrades metadata to ESRI ISO 19139 format, imports template metadata, removes geoprocessing history,
     and cleans the metadata of machine names before syncing back to the feature class or shapefile.
 
@@ -193,7 +199,7 @@ def import_and_clean_feature_metadata(in_feature: str, md_template_file: str):
 
     # import from metadata template
     item_md.importMetadata(
-        sourceUri=md_template_file, #metadata_import_option="ISO19139"
+        sourceUri=md_template_file,  # metadata_import_option="ISO19139"
     )
     logger.debug(f"Importing metadata from {md_template_file}")
 
@@ -211,9 +217,9 @@ def import_and_clean_feature_metadata(in_feature: str, md_template_file: str):
 
     # Overwrite XML with metadata cleaned of machine names
     item_md.saveAsXML(md_template_file, "REMOVE_MACHINE_NAMES")
-    
+
     # Create metadata object for cleaned XML and copy back to feature
     updated_md = md.Metadata(md_template_file)
 
-    item_md.copy(updated_md) # copy as opposed to importMetadata prevents paths from being reintroduced
+    item_md.copy(updated_md)  # copy as opposed to importMetadata prevents paths from being reintroduced
     item_md.save()
