45c16bd55c05651af39806f0c9a5f12f2c647479
Jack Rosacker <18175209+jackrosacker@users.noreply.github.com>
2026-09-04 11:18:00 -0400

    testing ci

---
+++ experiments/fail_ci.py
@@ -0,0 +1,29 @@
+import datetime
+import pandas
+import xarray
+import time
+
+
+def this_outputs_a_string() -> str:
+    output = {"key": "value", "and": "another"}
+    return output
+
+
+def this_accepts_an_integer(a: int, b: int) -> int:
+    sum = a + b
+    return sum
+
+
+def this_is_missing_a_return_type():
+    return "this is a lot of text that i'm hoping will trigger a ruff failure. maybe? ok, no we're exceeding most of the python line length limits i'm aware of"
+
+
+def main():
+    this_outputs_a_string()
+
+    print(this_accepts_an_integer(a="hello", b=2))
+
+    this_is_missing_a_return_type()
+
+if __name__ == "__main__":
+    main()
